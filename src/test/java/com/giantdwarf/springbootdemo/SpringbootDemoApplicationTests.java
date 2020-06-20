e21ee66c66ae3a1be7eb707c481108926ef3e28b
seungin yang <rhfpdk92@naver.com>
2020-06-20 23:20:31 +0900

    Initial commit

---
+++ src/test/java/com/giantdwarf/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giantdwarf.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
