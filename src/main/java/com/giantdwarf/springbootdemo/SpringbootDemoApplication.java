a79e5d87d48c7297ea752ef13335a07c0eff34b1
seungin yang <rhfpdk92@naver.com>
2020-06-22 18:16:36 +0900

    applied https and http2

--- src/main/java/com/giantdwarf/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/giantdwarf/springbootdemo/SpringbootDemoApplication.java
@@ -1,13 +1,27 @@
 package com.giantdwarf.springbootdemo;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootDemoApplication {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
+
 
 }
