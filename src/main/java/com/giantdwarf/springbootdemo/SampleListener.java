700dc1226d844fc2ce721b29c87bef63ff0947e8
seungin yang <rhfpdk92@naver.com>
2020-06-23 12:51:05 +0900

    Accessing Application arguments

---
+++ src/main/java/com/giantdwarf/springbootdemo/SampleListener.java
@@ -0,0 +1,13 @@
+package com.giantdwarf.springbootdemo;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent event) {
+        System.out.println("Application is starting");
+    }
+}
---
+++ src/main/java/com/giantdwarf/springbootdemo/SampleListener2.java
@@ -0,0 +1,15 @@
+package com.giantdwarf.springbootdemo;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleListener2 implements ApplicationListener<ApplicationStartedEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        System.out.println("Application started");
+
+    }
+}
