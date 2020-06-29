cc2ebef499ef67f5261373ffee1477d1c29786bc
seungin yang <rhfpdk92@naver.com>
2020-06-29 15:12:44 +0900

    Testing

---
+++ src/test/java/com/giantdwarf/springbootdemo/testing/SampleControllerTest.java
@@ -0,0 +1,59 @@
+package com.giantdwarf.springbootdemo.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // default
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // random port embedded tomcat
+@AutoConfigureMockMvc
+class SampleControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @MockBean
+    SampleService mockSampleService;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello seungin"))
+                .andDo(print());
+    }
+
+    @Test
+    public void hello2() throws Exception {
+        when(mockSampleService.getName()).thenReturn("seungin");
+
+//        String result = testRestTemplate.getForObject("/hello", String.class);
+//        assertThat(result).isEqualTo("hello seungin");
+
+        webTestClient.get().uri("/hello").exchange().expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello seungin");
+
+
+    }
+
+}
---
+++ src/test/java/com/giantdwarf/springbootdemo/testing/WebMvcTests.java
@@ -0,0 +1,45 @@
+package com.giantdwarf.springbootdemo.testing;
+
+import com.giantdwarf.springbootdemo.testing.SampleController;
+import com.giantdwarf.springbootdemo.testing.SampleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SampleController.class)
+@TestPropertySource(locations = "classpath:/test.properties")
+class WebMvcTests {
+
+    @MockBean
+    SampleService mockSampleService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("seungin");
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello seungin"))
+                .andDo(print());
+    }
+
+
+
+}
