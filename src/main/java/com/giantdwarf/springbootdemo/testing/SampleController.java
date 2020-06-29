db22d72cf1f571c13154a95f2100b21f881b434f
seungin yang <rhfpdk92@naver.com>
2020-06-29 15:38:06 +0900

    OutputCapture - console 탐색

--- src/main/java/com/giantdwarf/springbootdemo/testing/SampleController.java
+++ src/main/java/com/giantdwarf/springbootdemo/testing/SampleController.java
@@ -1,5 +1,7 @@
 package com.giantdwarf.springbootdemo.testing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
+    Logger logger = LoggerFactory.getLogger(SampleController.class);
+
     @Autowired
     private SampleService sampleService;
 
     @GetMapping("/hello")
     public String hello(){
+        logger.info("yang");
+        System.out.println("skip");
         return "hello "+sampleService.getName();
     }
 }
--- src/test/java/com/giantdwarf/springbootdemo/testing/WebMvcTests.java
+++ src/test/java/com/giantdwarf/springbootdemo/testing/WebMvcTests.java
@@ -3,12 +3,16 @@ package com.giantdwarf.springbootdemo.testing;
 import com.giantdwarf.springbootdemo.testing.SampleController;
 import com.giantdwarf.springbootdemo.testing.SampleService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SampleController.class)
-@TestPropertySource(locations = "classpath:/test.properties")
+@ExtendWith(OutputCaptureExtension.class)
 class WebMvcTests {
 
     @MockBean
@@ -31,13 +35,15 @@ class WebMvcTests {
     MockMvc mockMvc;
 
     @Test
-    public void hello() throws Exception {
+    public void hello(CapturedOutput output) throws Exception {
         when(mockSampleService.getName()).thenReturn("seungin");
 
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello seungin"))
                 .andDo(print());
+
+        assertThat(output).contains("yang").contains("skip");
     }
 
 
