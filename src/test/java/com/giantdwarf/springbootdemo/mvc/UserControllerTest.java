be03bb9fcf972b5841170db485d7a96eb81f86f2
seungin yang <rhfpdk92@naver.com>
2020-07-01 01:30:30 +0900

    HttpMessageConverters

---
+++ src/test/java/com/giantdwarf/springbootdemo/mvc/UserControllerTest.java
@@ -0,0 +1,44 @@
+package com.giantdwarf.springbootdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello2"));
+    }
+
+    @Test
+    public void createUser_Json() throws Exception {
+        String userJson = "{\"username\":\"seungin\", \"password\" :\"1234\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username", is(equalTo("seungin"))))
+                .andExpect(jsonPath("$.password", is((equalTo("1234")))))
+        ;
+
+    }
+
+
+}
