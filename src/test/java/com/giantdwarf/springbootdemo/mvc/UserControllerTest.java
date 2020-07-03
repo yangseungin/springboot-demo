696437b5fce94d166affc1e33fb3fc3967bd9429
seungin yang <rhfpdk92@naver.com>
2020-07-03 16:31:54 +0900

    add xml message converter

--- src/test/java/com/giantdwarf/springbootdemo/mvc/UserControllerTest.java
+++ src/test/java/com/giantdwarf/springbootdemo/mvc/UserControllerTest.java
@@ -40,5 +40,19 @@ class UserControllerTest {
 
     }
 
+    @Test
+    public void createUser_XML() throws Exception {
+        String userJson = "{\"username\":\"seungin\", \"password\" :\"1234\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_XML)
+                .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/User/username").string("seungin"))
+                .andExpect(xpath("/User/password").string("1234"))
+        ;
+
+    }
+
 
 }
