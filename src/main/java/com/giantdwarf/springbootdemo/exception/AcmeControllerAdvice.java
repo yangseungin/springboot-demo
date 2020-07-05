4b30c2c77fcf5ffff0918637fc77a1f5a4f93f2c
seungin yang <rhfpdk92@naver.com>
2020-07-05 19:58:35 +0900

    custom error page

---
+++ src/main/java/com/giantdwarf/springbootdemo/exception/AcmeControllerAdvice.java
@@ -0,0 +1,29 @@
+package com.giantdwarf.springbootdemo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+//@ControllerAdvice
+public class AcmeControllerAdvice extends ResponseEntityExceptionHandler {
+
+//    @ExceptionHandler(TestException.class)
+//    @ResponseBody
+    ResponseEntity<?> handleControllerException(HttpServletRequest request, Throwable ex) {
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(new CustomErrorType(status.value(), ex.getMessage()), status);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
+}
